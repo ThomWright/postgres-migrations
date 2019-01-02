0eeb4f2adf4740c35eaa5646e38fdccf55878cd5
Thom Wright <thom.wright@candide.eu>
2019-01-02 15:57:00 +0000

    Remove logs in tests

--- src/__unit__/migration-file/index.js
+++ src/__unit__/migration-file/index.js
@@ -7,8 +7,6 @@ test("Hashes of JS files should be the same when the SQL is the same", async t =
     migrationFile.load(__dirname + "/fixtures/different-js-same-sql-2/1_js.js"),
   ])
 
-  console.log(js1, js2)
-
   t.is(js1.hash, js2.hash)
 })
 
@@ -17,8 +15,6 @@ test("Hashes of JS files should be different when the SQL is different", async t
     migrationFile.load(__dirname + "/fixtures/same-js-different-sql-1/1_js.js"),
     migrationFile.load(__dirname + "/fixtures/same-js-different-sql-2/1_js.js"),
   ])
-
-  console.log(js1, js2)
 
   t.not(js1.hash, js2.hash)
 })
