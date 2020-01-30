6fd11cbf2e1f7870f82d31e6d7455e40b58ca218
Thom Wright <thom.wright@candide.eu>
2020-01-30 16:36:52 +0000

    Remove old eslint files

--- src/__tests__/fixtures/js-no-string-literal/1_almost-string.js
+++ src/__tests__/fixtures/js-no-string-literal/1_almost-string.js
@@ -1,4 +1,3 @@
 module.exports.generateSql = () => {
-  // eslint-disable-next-line no-new-wrappers
   return new String("I want to run!!")
 }
--- src/__unit__/migration-file/fixtures/inc.js
+++ src/__unit__/migration-file/fixtures/inc.js
@@ -1,3 +1,2 @@
 let i = 0
-// eslint-disable-next-line no-plusplus
 module.exports = () => (i++).toString()
