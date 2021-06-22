9d4d75749da92584232d024904cdf67ab5a4fb3d
Thom Wright <dev@thomwright.co.uk>
2021-06-22 18:39:28 +0100

    Update dependencies
    Run `npm update` and `npm audit fix`
    
    Fix formatting

--- src/run-migration.ts
+++ src/run-migration.ts
@@ -23,38 +23,36 @@ const insertMigration = async (
   return client.query(sql)
 }
 
-export const runMigration = (
-  migrationTableName: string,
-  client: BasicPgClient,
-  log: Logger = noop,
-) => async (migration: Migration) => {
-  const inTransaction =
-    migration.sql.includes("-- postgres-migrations disable-transaction") ===
-    false
+export const runMigration =
+  (migrationTableName: string, client: BasicPgClient, log: Logger = noop) =>
+  async (migration: Migration) => {
+    const inTransaction =
+      migration.sql.includes("-- postgres-migrations disable-transaction") ===
+      false
 
-  log(`Running migration in transaction: ${inTransaction}`)
+    log(`Running migration in transaction: ${inTransaction}`)
 
-  const begin = inTransaction ? () => client.query("START TRANSACTION") : noop
+    const begin = inTransaction ? () => client.query("START TRANSACTION") : noop
 
-  const end = inTransaction ? () => client.query("COMMIT") : noop
+    const end = inTransaction ? () => client.query("COMMIT") : noop
 
-  const cleanup = inTransaction ? () => client.query("ROLLBACK") : noop
+    const cleanup = inTransaction ? () => client.query("ROLLBACK") : noop
 
-  try {
-    await begin()
-    await client.query(migration.sql)
-    await insertMigration(migrationTableName, client, migration, log)
-    await end()
-
-    return migration
-  } catch (err) {
     try {
-      await cleanup()
-    } catch {
-      //
+      await begin()
+      await client.query(migration.sql)
+      await insertMigration(migrationTableName, client, migration, log)
+      await end()
+
+      return migration
+    } catch (err) {
+      try {
+        await cleanup()
+      } catch {
+        //
+      }
+      throw new Error(
+        `An error occurred running '${migration.name}'. Rolled back this migration. No further migrations were run. Reason: ${err.message}`,
+      )
     }
-    throw new Error(
-      `An error occurred running '${migration.name}'. Rolled back this migration. No further migrations were run. Reason: ${err.message}`,
-    )
   }
-}
